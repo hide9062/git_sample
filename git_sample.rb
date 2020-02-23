ebadf75ff143478e05fadedcf8ca044a866c29ad
hede9062 <hide@hidenoMacBook-Air.local>
2020-02-23 15:57:12 +0900

    introduce

--- git_sample.rb
+++ git_sample.rb
@@ -3,24 +3,32 @@ class User
 
   def initialize(**params)
     @name = params[:name]
+    @age = params[:age]
+  end
+
+  def introduce
+    if @age > 20
+      "初めまして、私の名前は#{@name}です"
+    else
+      "僕は#{@name}だよ!!"
+    end
   end
 end
 
-def build_users(names)
+def build_users(params)
   users = []
-  names.each do |name|
-    users << User.new(name: name)
+  params.each do |params|
+    users << User.new(params)
   end
 
-users
+  users
 end
 
-names = ["saitou", "taira", "yamada"]
-users = build_users(names)
+params = [{name: "saitou", age: 22}, {name: "taira", age: 12}, {name: "yamada", age: 99}]
 
-users = build_users(names)
-
+users = build_users(params)
 
 users.each do |user|
   puts user.name
+  puts user.introduce
 end
