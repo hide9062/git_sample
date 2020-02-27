9fd793017952f352debdbaaa5b0926f54c34035e
hede9062 <hide@hidenoMacBook-Air.local>
2020-02-28 01:48:07 +0900

    高橋さんを追加

--- git_sample.rb
+++ git_sample.rb
@@ -48,7 +48,7 @@ def build_users(params)
   users
   end
 
-  params = [{name: "saitou", age: 22}, {name: "taira", age: 12}, {name: "yamada", age: 99}]
+  params = [{name: "saitou", age: 22}, {name: "taira", age: 12}, {name: "yamada", age: 99}, {name: "takahasi", age: 54}]
 
   users = build_users(params)
 
